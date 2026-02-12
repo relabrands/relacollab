2ceebff03cf529bba5f8376b06afa72df1fe75c5
Robinson Sanchez <relabrands@gmail.com>
2026-02-11 21:55:21 -0400

    2 video por reels, poder seleccionar plataforma en createe campaing, filtrar por plataforma en brand, etc

--- src/pages/brand/BrandAnalytics.tsx
+++ src/pages/brand/BrandAnalytics.tsx
@@ -354,3 +354,10 @@ export default function BrandAnalytics() {
             </div>
         );
     }
+                        </div >
+                    )
+}
+                </main >
+            </div >
+        );
+    }
